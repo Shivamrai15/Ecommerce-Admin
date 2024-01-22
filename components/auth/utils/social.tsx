198ef9c870f2ea02c9d59ff5a005d7b474a9c4f8
Shivam <shivamrai6836@gmail.com>
2024-01-22 23:35:22 +0530

    Google Oauth login

--- components/auth/utils/social.tsx
+++ components/auth/utils/social.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import { Button } from "@/components/ui/button"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 
 export const Social = () => {
+
+    const onClick = () => {
+        signIn("google", {
+            callbackUrl : DEFAULT_LOGIN_REDIRECT
+        });
+    }
+
     return (
         <Button
+            onClick={onClick}
             className="w-full"
             variant="outline"
         >
